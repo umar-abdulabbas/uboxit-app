8f03920c3767a827d5ecf9a8d0f61781e11821ab
umar <text.umarabbas@gmail.com>
2018-03-13 19:38:55 +0100

    Uboxit App - Setup files

---
+++ app/src/main/java/com/example/app/MyAppWebViewClient.java
@@ -0,0 +1,20 @@
+package com.example.app;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MyAppWebViewClient extends WebViewClient {
+
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        if (Uri.parse(url).getHost().endsWith("http://188.226.190.20/home")) {
+            return false;
+        }
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        view.getContext().startActivity(intent);
+        return true;
+    }
+}
